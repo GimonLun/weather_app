15f76742e4a3ffde84dbfcd017fbe26abed2eefe
Gimon <yunlun.tan@cognixus.com>
2022-01-22 18:44:34 +0800

    Update weather rest client and weather data

---
+++ lib/data/models/weathers/api/response/weather_details_response.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:weather_app/data/models/weathers/main.dart';
+import 'package:weather_app/data/models/weathers/summary.dart';
+import 'package:weather_app/data/models/weathers/winds.dart';
+
+part 'weather_details_response.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class WeatherDetailsResponse extends Equatable {
+  final String message;
+  final String cod;
+
+  final Main main;
+  final List<Summary> weather;
+  final Winds wind;
+
+  const WeatherDetailsResponse({
+    required this.message,
+    required this.cod,
+    required this.main,
+    required this.weather,
+    required this.wind,
+  });
+
+  factory WeatherDetailsResponse.fromJson(Map<String, dynamic> json) => _$WeatherDetailsResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherDetailsResponseToJson(this);
+
+  @override
+  List<Object> get props => [
+        message,
+        cod,
+        main,
+        weather,
+        wind,
+      ];
+}
