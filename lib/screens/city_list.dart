60638c3e9513285fe3ba206e43a6e4306a7c52e2
Gimon <yunlun.tan@cognixus.com>
2022-01-22 08:44:00 +0800

    Basic Setup for Material App
    Key changes:
    - Create empty city list page
    - Remove unused comment and description

---
+++ lib/screens/city_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CityList extends StatefulWidget {
+  const CityList({Key? key}) : super(key: key);
+
+  @override
+  _CityListState createState() => _CityListState();
+}
+
+class _CityListState extends State<CityList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('City List'),
+      ),
+    );
+  }
+}
